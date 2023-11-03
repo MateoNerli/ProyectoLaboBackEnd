5caac98b0616b9c9e828e4bda51e2f7e18753c63
MateoNerli <mnerli2003@gmail.com>
2023-11-03 20:20:02 -0300

    Correccion de cors, y eliminacion de startup

--- ProyectoLaboBackEnd/Program.cs
+++ ProyectoLaboBackEnd/Program.cs
@@ -1,29 +1,109 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using ProyectoLaboBackEnd.Config;
+using ProyectoLaboBackEnd.Repositories;
+using ProyectoLaboBackEnd.Services;
 using ProyectoLaboBackEnd;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Text;
+using ProyectoLaboBackEnd.Models;
 
+var builder = WebApplication.CreateBuilder(args);
 
-namespace ProyectoLaboBackEnd
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
 {
-    public class Program
+    options.SwaggerDoc("v1", new OpenApiInfo
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        Version = "v1",
+        Title = "Users API",
+        Description = "An ASP.NET Core Web API for Reddit clone",
+    });
+    options.AddSecurityDefinition("Token", new OpenApiSecurityScheme()
+    {
+        BearerFormat = "JWT",
+        Description = "JWT Authorization header using the Bearer scheme.",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer"
+    });
+    options.OperationFilter<JwtAuthOperationsFilter>();
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-
-                });
+// services
+builder.Services.AddScoped<UserService>();
+builder.Services.AddScoped<PostService>();
+builder.Services.AddScoped<IEncoderService, EncoderService>();
+builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped<RoleService>();
+// db
+builder.Services.AddDbContext<proyectolabo4Context>(options =>
+{
+    if (builder.Environment.IsDevelopment())
+    {
+        var conn = builder.Configuration.GetConnectionString("DefaultConnection");
+        options.UseMySql(conn, ServerVersion.AutoDetect(conn));
     }
+    else
+    {
+        var conn = builder.Configuration.GetConnectionString("ProdConnection");
+        options.UseMySql(conn, ServerVersion.AutoDetect(conn));
+    }
+});
+
+// automapper
+builder.Services.AddAutoMapper(typeof(Mapping));
+// repostories
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IPostRepository, PostRepository>();
+builder.Services.AddScoped<IRoleRepository, RoleRepository>();
+
+// secret key
+var secretKey = builder.Configuration.GetSection("jwtSettings").GetSection("secretKey").ToString();
+
+// jwt
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.SaveToken = true;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey)),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+        };
+    });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseCors(options =>
+{
+    options.AllowAnyOrigin();
+    options.AllowAnyMethod();
+    options.AllowAnyHeader();
+});
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
