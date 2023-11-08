8767c766fdec35edaf649cc2c290417c75ba8b85
MateoNerli <mnerli2003@gmail.com>
2023-11-08 14:47:22 -0300

    Correccion script

--- BaseDeDatos/ScriptProyectoLabo.sql
+++ BaseDeDatos/ScriptProyectoLabo.sql
@@ -30,11 +30,13 @@ CREATE TABLE `comments` (
   `CreatedAt` datetime DEFAULT CURRENT_TIMESTAMP,
   `DeletedAt` datetime DEFAULT NULL,
   `UpdateAt` datetime DEFAULT NULL,
+  `CommentId` int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`CommentId`),
   KEY `PostId` (`PostId`),
   KEY `UserId` (`UserId`),
   CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`PostId`) REFERENCES `posts` (`PostId`),
   CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`UserId`) REFERENCES `users` (`UserId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -43,7 +45,7 @@ CREATE TABLE `comments` (
 
 LOCK TABLES `comments` WRITE;
 /*!40000 ALTER TABLE `comments` DISABLE KEYS */;
-INSERT INTO `comments` VALUES (1,2,'¡Gran publicación del post 1!',NULL,'2023-11-01 21:08:09',NULL,NULL),(1,2,'¡Gran publicación del post 1!',NULL,'2023-11-01 21:08:10',NULL,NULL),(2,1,'Esto es interesante.',NULL,'2023-11-01 21:08:12',NULL,NULL);
+INSERT INTO `comments` VALUES (1,2,'¡Gran publicación del post 1!',NULL,'2023-11-01 21:08:09',NULL,NULL,1),(1,2,'¡Gran publicación del post 1!',NULL,'2023-11-01 21:08:10',NULL,NULL,2),(2,1,'Esto es interesante.',NULL,'2023-11-01 21:08:12',NULL,NULL,3),(1,3,'creato','creato','2023-11-07 19:19:43',NULL,NULL,4);
 /*!40000 ALTER TABLE `comments` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -59,7 +61,7 @@ CREATE TABLE `communities` (
   `Name` varchar(30) NOT NULL,
   `State` tinyint(1) NOT NULL,
   PRIMARY KEY (`CommunityId`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -68,7 +70,7 @@ CREATE TABLE `communities` (
 
 LOCK TABLES `communities` WRITE;
 /*!40000 ALTER TABLE `communities` DISABLE KEYS */;
-INSERT INTO `communities` VALUES (1,'Comunidad 1',1),(2,'Comunidad 2',0),(3,'Comunidad 3',1);
+INSERT INTO `communities` VALUES (1,'Comunidad 1',1),(2,'Comunidad 2',0),(3,'Comunidad 3',1),(4,'Deporte',1);
 /*!40000 ALTER TABLE `communities` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -122,7 +124,7 @@ CREATE TABLE `posts` (
   KEY `CommunityId` (`CommunityId`),
   CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`UserId`) REFERENCES `users` (`UserId`),
   CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`CommunityId`) REFERENCES `communities` (`CommunityId`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -131,7 +133,7 @@ CREATE TABLE `posts` (
 
 LOCK TABLES `posts` WRITE;
 /*!40000 ALTER TABLE `posts` DISABLE KEYS */;
-INSERT INTO `posts` VALUES (1,1,'2023-11-01 12:00:00','Mi primera publicación','Este es el contenido de mi publicación.','imagen.jpg',1,'2023-11-01 21:06:50',NULL,NULL),(2,2,'2023-11-01 14:30:00','Otra publicación interesante','Aquí va el contenido de otra publicación.',NULL,2,'2023-11-01 21:07:12',NULL,NULL);
+INSERT INTO `posts` VALUES (1,1,'2023-11-01 12:00:00','Mi primera publicación','Este es el contenido de mi publicación.','imagen.jpg',1,'2023-11-01 21:06:50',NULL,NULL),(2,2,'2023-11-01 14:30:00','Otra publicación interesante','Aquí va el contenido de otra publicación.',NULL,2,'2023-11-01 21:07:12',NULL,NULL),(3,1,'0001-01-01 00:00:00','Public creada aca','Public creada aca','Public creada aca',1,'2023-11-03 21:31:58',NULL,NULL),(4,1,'0001-01-01 00:00:00',' segunda Public creada aca',' segunda  Public creada aca','segunda  Public creada aca',1,'2023-11-03 21:32:33',NULL,NULL),(5,3,'0001-01-01 00:00:00','Natali el mas capito','NAtali callate la boca','dale BBOCA.PNG',2,'2023-11-04 13:41:38',NULL,NULL),(7,3,'0001-01-01 00:00:00','Hoilka','vsfsfsdfsf','dfsdfs',1,'2023-11-07 00:33:58',NULL,NULL);
 /*!40000 ALTER TABLE `posts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -209,7 +211,7 @@ CREATE TABLE `userroles` (
 
 LOCK TABLES `userroles` WRITE;
 /*!40000 ALTER TABLE `userroles` DISABLE KEYS */;
-INSERT INTO `userroles` VALUES (1,1),(1,2),(2,3);
+INSERT INTO `userroles` VALUES (3,1),(4,1),(5,1),(6,1);
 /*!40000 ALTER TABLE `userroles` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -237,7 +239,7 @@ CREATE TABLE `users` (
   PRIMARY KEY (`UserId`),
   UNIQUE KEY `UserName` (`UserName`),
   UNIQUE KEY `Email` (`Email`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -246,7 +248,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Jane','Smith','janesmith','123456','jane.smith@example.com','2023-11-01','profile_pic.jpg','1985-03-15','987-654-3210','2023-11-01 22:13:04',NULL,NULL),(2,'John','Doe','johndoe','123456','john.doe@example.com','2023-11-01','profile_pic.jpg','1990-01-01','123-456-7890','2023-11-01 22:13:04',NULL,NULL);
+INSERT INTO `users` VALUES (1,'Jane','Smith','janesmith','123456','jane.smith@example.com','2023-11-01','profile_pic.jpg','1985-03-15','987-654-3210','2023-11-01 22:13:04',NULL,NULL),(2,'John','Doe','johndoe','123456','john.doe@example.com','2023-11-01','profile_pic.jpg','1990-01-01','123-456-7890','2023-11-01 22:13:04',NULL,NULL),(3,'Mateo','Nerli','Mateo','$2a$13$rLFgXICmj/9GXuoy8V.0zuJ.xuS/TCToAgIxdl4QoLlip.4WFAdbS','mateo@gmail.com','0001-01-01','imagen.png','2003-03-06','3364512460','2023-11-02 22:05:04',NULL,NULL),(4,'useractualizado','useractualizado','useractualizado','$2a$13$jD8l29auc8/lVfzeAB3q..16XDTxtOIXqKqmQ9DkLbSWaLMYKTDBK','useractualizado@useractualizado.com','0001-01-01','useractualizado','2023-11-04','useractualizado','2023-11-03 21:23:19',NULL,NULL),(5,'usercreate','usercreate','usercreate','$2a$13$cjRXFe3HzPkPc1Xw1ZdiqeN8bRUpgwNSrHkS4DG0k8ZXk33Y1Cf7K','usercreate@gmail.com','0001-01-01','usercreate','2023-11-04','123456','2023-11-03 21:38:11',NULL,NULL),(6,'Mateo','Nerli','MNErli','$2a$13$moqaOnqgoTnK0hPwzZBqfOPYuw5MQQs4QQ4pG5RMcf0mzAz.vU/BC','mateone@gmail.com','0001-01-01','foto.png','2023-11-07','3366545545','2023-11-07 20:55:38',NULL,NULL);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -259,4 +261,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-11-01 22:25:27
+-- Dump completed on 2023-11-08 14:46:44
