1e0afbbdc3cd92ff10dd8953e8141e8be04048eb
juan <juanbejamingiupponi@gmail.com>
2023-11-09 09:24:50 -0300

    UpdateDto null

--- ProyectoLaboBackEnd/Models/User/Dto/UpdateUserDto.cs
+++ ProyectoLaboBackEnd/Models/User/Dto/UpdateUserDto.cs
@@ -5,15 +5,15 @@ namespace ProyectoLaboBackEnd.Models.User.Dto
     public class UpdateUserDto
     {
         [MaxLength(30)]
-        public string Name { get; set; } = null!;
+        public string? Name { get; set; } 
         [MaxLength(40)]
-        public string LastName { get; set; } = null!;
+        public string? LastName { get; set; } 
         [MaxLength(40)]
-        public string UserName { get; set; } = null!;
+        public string? UserName { get; set; }
         [EmailAddress]
-        public string Email { get; set; } = null!;
+        public string? Email{ get; set; } 
         [MaxLength(40)]
-        public string Pfp { get; set; } = null!;
+        public string? Pfp { get; set; }
         [MaxLength(40)]
         public string? Phone { get; set; }
     }
